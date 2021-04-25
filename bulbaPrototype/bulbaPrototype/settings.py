8c5816ae114e55f76bf9e75ca1d220478b065c70
Jerome Johnson <jerome.j.johnson@outlook.com>
2021-04-25 19:51:46 -0400

    add gitignore

--- bulbaPrototype/bulbaPrototype/settings.py
+++ bulbaPrototype/bulbaPrototype/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!2b#v#ierh0whf!re$=0!g#-fdk936ba00@)9aohsvsq=b^8+4'
+SECRET_KEY ='secret.env'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bulba',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
